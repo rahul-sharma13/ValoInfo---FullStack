88bf0faac2e3d6b044ca7d1663bd29b9290f6ea4
rahul-sharma13 <rahulharit13@gmail.com>
2024-07-25 15:14:21 +0530

    change 2

--- api/index.js
+++ api/index.js
@@ -1,14 +1,57 @@
-import { app } from "./app.js";
 import dotenv from "dotenv";
 import connectDb from "./src/db/index.js";
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+// routes
+import eventRouter from "./src/routes/Events.routes.js";
+import authRouter from "./src/routes/Auth.routes.js";
+import userRouter from "./src/routes/User.routes.js";
+import postRouter from "./src/routes/Post.routes.js";
+import commentRouter from "./src/routes/Comment.routes.js";
+import articleRouter from "./src/routes/Article.routes.js";
 
 dotenv.config({
   path: "./env",
 });
 
+const app = express();
+
 app.get("/", (req, res) => {
   res.send("server is up index!");
 });
+
+app.use(
+  cors({
+    origin: "http://localhost:5173" || process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(express.json({ limit: "50mb" })); // limit of acceptance of json data
+app.use(
+  express.urlencoded({ extended: true, limit: "50mb", parameterLimit: 50000 })
+);
+app.use(express.static("public"));
+app.use(cookieParser());
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
+// handling the routes
+app.use("/api/v1/event", eventRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/post", postRouter);
+app.use("/api/v1/comment", commentRouter);
+app.use("/api/v1/article", articleRouter);
 
 connectDb()
   .then(() => {
@@ -24,3 +67,4 @@ connectDb()
   .catch((err) => {
     console.log("MONGODB CONNECTION FAILED!! : ", err);
   });
+
