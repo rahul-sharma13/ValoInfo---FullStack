6daae46b9ba7796c8568d2ecb1ccecfc4a352539
Rahul Sharma <115665124+rahul-sharma13@users.noreply.github.com>
2024-07-26 01:53:55 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ app.get("/", (req, res) => {
 
 app.use(
   cors({
-    origin: "http://localhost:5173" || process.env.CORS_ORIGIN,
+    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
     credentials: true,
   })
 );
