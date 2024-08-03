a0aa872d123132bbee1f5e31d32977f90011976a
rahul-sharma13 <rahulharit13@gmail.com>
2024-08-03 13:39:28 +0530

    for deploy

--- api/index.js
+++ api/index.js
@@ -25,11 +25,10 @@ app.get("/", (req, res) => {
 // middleware setup
 app.use(
   cors({
-    origin: [process.env.CORS_ORIGIN, "http://localhost:5173"],
+    origin: ["https://valo-info-c.vercel.app", "http://localhost:5173"],
     credentials: true,
   })
 );
-console.log(process.env.CORS_ORIGIN);
 app.use(express.json({ limit: "50mb" })); // limit of acceptance of json data
 app.use(
   express.urlencoded({ extended: true, limit: "50mb", parameterLimit: 50000 })
